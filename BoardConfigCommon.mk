0e32d579f83f55fa2c50f64d3034dae55d119012
Christopher N. Hesse <raymanfx@gmail.com>
2017-06-27 20:56:55 +0200

    hero: Configure recovery

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -122,3 +122,23 @@ TARGET_SCREEN_WIDTH := 1536
 
 # Release tools
 TARGET_RELEASETOOLS_EXTENSIONS := $(LOCAL_PATH)
+
+# Recovery
+#RECOVERY_VARIANT := twrp
+BOARD_HAS_DOWNLOAD_MODE := true
+TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/ramdisk/fstab.samsungexynos8890
+
+# TWRP
+ifeq ($(RECOVERY_VARIANT),twrp)
+TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/recovery/twrp.fstab
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+TW_THEME := portrait_hdpi
+TW_BRIGHTNESS_PATH := /sys/class/backlight/panel/brightness
+TW_MAX_BRIGHTNESS := 180
+TW_NO_REBOOT_BOOTLOADER := true
+TW_INCLUDE_CRYPTO := true
+TW_INCLUDE_NTFS_3G := true
+TW_HAS_DOWNLOAD_MODE := true
+TW_NO_EXFAT_FUSE := true
+TW_EXCLUDE_SUPERSU := true
+endif
