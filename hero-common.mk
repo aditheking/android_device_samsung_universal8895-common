903b8c5234722bc63748b6b528d4168d6c8054c0
Christopher N. Hesse <raymanfx@gmail.com>
2017-06-27 21:09:27 +0200

    hero: Add radio packages

--- hero-common.mk
+++ hero-common.mk
@@ -92,3 +92,22 @@ PRODUCT_PACKAGES += \
 # hardware/samsung/AdvancedDisplay (MDNIE)
 PRODUCT_PACKAGES += \
     AdvancedDisplay
+
+# Radio
+PRODUCT_PACKAGES += \
+    libxml2 \
+    libprotobuf-cpp-full
+
+PRODUCT_PACKAGES += \
+    libsecril-client \
+    libsecril-client-sap \
+    modemloader
+
+PRODUCT_PACKAGES += \
+    SamsungServiceMode
+
+# IPv6
+PRODUCT_PACKAGES += \
+    ebtables \
+    ethertypes \
+    libebtc
