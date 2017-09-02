2ed32b18aaf94e0dbd2331772ef9968e7107e5d7
Fevax <fer.vonarx@gmail.com>
2017-09-02 17:36:40 -0300

    Universal8895: Ramdisk update

--- ramdisk/Android.mk
+++ ramdisk/Android.mk
@@ -16,10 +16,10 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE		:= fstab.samsungexynos8890
+LOCAL_MODULE		:= fstab.samsungexynos8895
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= BOOT
-LOCAL_SRC_FILES		:= fstab.samsungexynos8890
+LOCAL_SRC_FILES		:= fstab.samsungexynos8895
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
@@ -80,18 +80,18 @@ LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE		:= init.samsungexynos8890.rc
+LOCAL_MODULE		:= init.samsungexynos8895.rc
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= BOOT
-LOCAL_SRC_FILES		:= init.samsungexynos8890.rc
+LOCAL_SRC_FILES		:= init.samsungexynos8895.rc
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE		:= init.samsungexynos8890.usb.rc
+LOCAL_MODULE		:= init.samsungexynos8895.usb.rc
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= BOOT
-LOCAL_SRC_FILES		:= init.samsungexynos8890.usb.rc
+LOCAL_SRC_FILES		:= init.samsungexynos8895.usb.rc
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
@@ -104,9 +104,9 @@ LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE		:= ueventd.samsungexynos8890.rc
+LOCAL_MODULE		:= ueventd.samsungexynos8895.rc
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= BOOT
-LOCAL_SRC_FILES		:= ueventd.samsungexynos8890.rc
+LOCAL_SRC_FILES		:= ueventd.samsungexynos8895.rc
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
